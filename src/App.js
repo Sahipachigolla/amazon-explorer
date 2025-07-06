b7261c96bdb737cae35c57a52ba88b498657ab0e
SahithiPachigolla <sahithipachigolla.work@gmail.com>
2025-07-06 23:36:23 +0530

    Initial commit: Tailwind setup working

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        return (
+    <div className="h-screen flex items-center justify-center bg-green-200">
+      <h1 className="text-4xl font-bold text-red-600">Tailwind is working!</h1>
+    </div>
+  );
+
+      </header>
+    </div>
+  );
+}
+  
+
+export default App;
